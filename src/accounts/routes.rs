80301abbdc5d888aaa78ffdc0847549eba05b56b
bvvn1 <bvvn@protonmail.com>
2025-09-18 13:26:14 +0300

    reworked db connections

--- src/accounts/routes.rs
+++ src/accounts/routes.rs
@@ -1,7 +1,8 @@
 use actix_web::{Responder, post, web};
+use sqlx::{PgPool, Pool, Postgres};
 
 #[post("/signup")]
-pub async fn user_signup() -> impl Responder {}
+pub async fn user_signup(db: web::Data<Pool<Postgres>>) -> impl Responder {}
 
 pub async fn accounts_scope() -> actix_web::Scope {
     web::scope("/account").service(user_signup)
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use actix_web::{App, HttpServer, web};
+use sqlx::{PgPool, postgres::PgPoolOptions};
 
-use crate::accounts::routes::accounts_scope;
+use crate::{accounts::routes::accounts_scope, db::connect_db};
 
 pub mod accounts;
-pub mod db;
 pub mod transactions;
 
 #[actix_web::main]
 async fn main() {
-    HttpServer::new(|| App::new().service(accounts_scope()))
-        .bind("127.0.0.1", 8080)?
-        .run()
+    let url = "postgres://postgres:postgres@db:5432/finance";
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(url)
         .await
+        .expect("error initializing pool");
+
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .service(accounts_scope())
+    })
+    .bind("127.0.0.1", 8080)?
+    .run()
+    .await
 }
